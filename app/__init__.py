add521fc0d140215120db0eaf0da52826342089d
PierinaTufillaro <pierinatufillaro@hotmail.com>
2025-08-26 16:06:23 -0300

    Preparing for deploy

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from .extensions import db
 from .routes.client_routes import client_routes
 from .routes.admin_routes import admin_routes
 from .routes.auth_routes import auth_routes
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_talisman import Talisman
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
@@ -14,6 +17,9 @@ from sqlalchemy.engine import Engine
 import sqlite3
 
 load_dotenv()
+limiter = Limiter(key_func=get_remote_address)
+talisman = Talisman()
+
 
 def create_app():
     """Crea y configura la aplicación Flask."""
@@ -27,7 +33,15 @@ def create_app():
 
     # Inicialización de extensiones
     db.init_app(app)
-
+    limiter.init_app(app)
+    csp = {
+        'default-src': "'self'",
+        'style-src': ["'self'", "https://cdn.jsdelivr.net"],
+        'font-src': ["'self'", "https://cdn.jsdelivr.net"],
+        'script-src': ["'self'", "https://cdn.jsdelivr.net"]
+    }
+    Talisman(app, content_security_policy=csp)
+    
     # Activar foreign keys en SQLite
     @event.listens_for(Engine, "connect")
     def set_sqlite_pragma(dbapi_connection, connection_record):
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 8000))
+    debug = os.getenv("FLASK_ENV", "dev") == "dev"
+    app.run(host="0.0.0.0", port=port, debug=debug)
